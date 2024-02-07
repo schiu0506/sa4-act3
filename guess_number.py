d0915741f99327249151e5c71f07dc0a4a4585a7
Shuwan Chiu <kori1468573088@gmail.com>
2024-02-07 16:02:49 -0600

    Added user prompts until they guess the correct number or enter q to exit

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,20 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
 
-if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+while True:
+    guess = input("What number am I thinking of? (Enter 'q' to quit) ")
+
+    if guess == 'q':
+        print(f"The number was {number}.")
+        break
+
+    try:
+        guess = int(guess)
+        if guess == number:
+            print("Congratulations! You guessed the right number.")
+            break
+        else:
+            print("Sorry! That's not the correct number. Try again.")
+    except ValueError:
+        print("Please enter a valid number or 'q' to quit.")
